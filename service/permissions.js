f73e2a013a4af886c297d88976887ff38faf2312
lgoicoechea <lucasgoicoechea@gmail.com>
2023-02-20 13:37:44 -0300

    d

--- service/permissions.js
+++ service/permissions.js
@@ -20,7 +20,6 @@ const service = {
     .populate({path: 'perm', model: 'Perm'})
   }),
   listPermsForRole: async(function (id) {
-   
     const role = awaitFor(RoleModel.findOne({'code': id}));
     const permsRole = awaitFor(PermRoleModel
       .find({'role': role._id})
@@ -29,25 +28,33 @@ const service = {
       .then( (permroled) => {
         let permCheckd = [];
         let permrolecheck = PermRoleChecked;
-        permroled.array.forEach(element => {
-          permrolecheck._id = element._id;
-          permrolecheck.perm._id = element.perm._id; 
-          permCheckd.push(permrolecheck);
-         });
+        if (permroled !== undefined) {
+          permroled.forEach(element => {
+            permrolecheck._id = element._id;
+            permrolecheck.perm._id = element.perm._id;
+            permrolecheck.perm.description = element.perm.description; 
+            permrolecheck.perm.code = element.perm.code; 
+            permCheckd.push(permrolecheck);
+           });
+        }        
+        return permCheckd;
       }));
-    const permssUser =  awaitFor(PermUsuarioModel.find({user: idUser})
-    .populate({path: 'user', model: 'User'})
-    .populate({path: 'perm', model: 'Perm'})
-    .then( (permuser) => {
-      let permCheckd = [];
-      let permrolecheck = PermRoleChecked;
-       permuser.array.forEach(element => {
-        permrolecheck._id = element._id;
-        permrolecheck.perm._id = element.perm._id; 
-        permCheckd.push(permrolecheck);
-       });
-    })
-    );
+    if (permsRole.lenght > 0 ) {
+      const permssUser =  awaitFor(PermUsuarioModel.find({user: idUser})
+      .populate({path: 'user', model: 'User'})
+      .populate({path: 'perm', model: 'Perm'})
+      .then( (permuser) => {
+        return permuser;
+      })
+      );
+      if (permssUser) {
+        permsRole.forEach( pR => {
+            pR.checked = permssUser.filter(t => t.perm._id == pR.perm._id).lenght > 0
+        })
+      }
+    }
+    
+    return permsRole;
   }),
   removePermission: async(function (id) {
     return PermUsuarioModel.remove({_id: id})
